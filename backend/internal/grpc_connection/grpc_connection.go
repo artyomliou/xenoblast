7a891ee56fcd76f6c86b91d33aec7212fb4b981f
Artyom Liou <artyomliou@gmail.com>
2024-11-17 21:06:17 +0800

    feat: client of game service always take specific addr

--- backend/internal/grpc_connection/grpc_connection.go
+++ backend/internal/grpc_connection/grpc_connection.go
@@ -3,6 +3,8 @@ package grpc_connection
 import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/resolver/manual"
 )
 
 func NewGrpcConnection(addr string) (*grpc.ClientConn, error) {
@@ -10,4 +12,20 @@ func NewGrpcConnection(addr string) (*grpc.ClientConn, error) {
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	}
 	return grpc.NewClient(addr, opts...)
+}
+
+func NewGrpcConnectionWithSpecificAddr(addr string) (*grpc.ClientConn, error) {
+	mr := manual.NewBuilderWithScheme("manual")
+
+	mr.InitialState(resolver.State{
+		Endpoints: []resolver.Endpoint{
+			{Addresses: []resolver.Address{{Addr: addr}}},
+		},
+	})
+
+	return grpc.NewClient(
+		mr.Scheme()+":///",
+		grpc.WithResolvers(mr),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
 }
