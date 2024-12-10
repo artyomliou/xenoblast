2b0168a5fea9c9aebed7a60ac0d72d913d05fbe5
Artyom Liou <artyomliou@gmail.com>
2024-12-10 15:54:11 +0800

    fix: new ECR repos

--- deploy/terraform/ecr.tf
+++ deploy/terraform/ecr.tf
@@ -1,11 +1,20 @@
 locals {
+  repository = [
+    "api-gateway",
+    "backend",
+    "http-service",
+    "websocket-service",
+    "auth-service",
+    "matchmaking-service",
+    "game-service",
+  ]
   images = [
     "api-gateway",
     "backend",
   ]
 }
 resource "aws_ecr_repository" "repos" {
-  for_each             = toset(local.images)
+  for_each             = toset(local.repository)
   name                 = "${var.project_name}-${each.value}"
   image_tag_mutability = "MUTABLE"
   force_delete         = true
@@ -20,4 +29,9 @@ data "aws_ecr_image" "images" {
 locals {
   api_gateway_image = data.aws_ecr_image.images["api-gateway"].image_uri
   backend_image     = data.aws_ecr_image.images["backend"].image_uri
+  # http_service_image        = data.aws_ecr_image.images["http-service"].image_uri
+  # websocket_service_image   = data.aws_ecr_image.images["websocket-service"].image_uri
+  # auth_service_image        = data.aws_ecr_image.images["auth-service"].image_uri
+  # matchmaking_service_image = data.aws_ecr_image.images["matchmaking-service"].image_uri
+  # game_service_image        = data.aws_ecr_image.images["game-service"].image_uri
 }
